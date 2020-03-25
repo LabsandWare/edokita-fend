0f519d67b986cfbdacca1062eb465d2ade08049b
smartyinkuse1 <olayinkafamutimi@gmail.com>
2020-03-25 20:37:41 +0000

    soft

--- projects/admin/src/app/layouts/admin-layout/admin-layout-routing.ts
+++ projects/admin/src/app/layouts/admin-layout/admin-layout-routing.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../pages/dashboard/dashboard.component';
 import { UserComponent } from '../pages/user/user.component';
+import { ProviderCheckerComponent } from '../pages/provider-checker/provider-checker.component';
 
 export const AdminLayoutRoutes: Routes = [
   { path: 'dashboard',      component: DashboardComponent },
-  { path: 'user',           component: UserComponent }
+  { path: 'user',           component: UserComponent },
+  { path: 'provider-checker', component: ProviderCheckerComponent}
 ];
---
+++ projects/admin/src/app/layouts/pages/provider-checker/provider-checker.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'user-cmp',
+    moduleId: module.id,
+    templateUrl: './provider-checker.component.html'
+})
+
+export class ProviderCheckerComponent implements OnInit{
+    ngOnInit(){
+    }
+}
