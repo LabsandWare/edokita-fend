3128506f328b2ab9dbcda424dbb88bc7e7afb5c8
dtytomi <dtytomi@gmail.com>
2020-03-29 21:54:11 +0100

    conflict resolved

--- projects/doctor/src/app/layouts/pages/register/register.component.ts
+++ projects/doctor/src/app/layouts/pages/register/register.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
+  moduleId: module.id,
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
